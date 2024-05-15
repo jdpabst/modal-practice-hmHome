59432059badeb86f2feb95f8ed258b9f8f32e52a
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2024-05-15 15:16:08 -0600

    set up the landing page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './Components/Header/Header';
+import LandingPage from './Components/LandingPage/LandingPage';
 import Footer from './Components/Footer/Footer';
 import './App.css';
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <LandingPage />
       <Footer />
     </div>
   );
---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+
+
+function LandingPage () {
+
+    return (
+        <div>
+            THIS IS THE LANDING PAGE
+        </div>
+    )
+}
+
+export default LandingPage;
